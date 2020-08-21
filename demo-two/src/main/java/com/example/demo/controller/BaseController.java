94c5e5f2617954ed7b6f1c2e644b45b096f60dfe
彭文彬 <925234379@qq.com>
2020-08-21 17:55:27 +0800

    学习JUC

--- demo-two/src/main/java/com/example/demo/controller/BaseController.java
+++ demo-two/src/main/java/com/example/demo/controller/BaseController.java
@@ -1,14 +1,38 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+
 /**
  * @description: BaseController <br>
  * @date: 2020/1/10 14:33 <br>
  * @author: PWB <br>
  * @version: 1.0 <br>
  */
+@Slf4j
 public class BaseController {
 
 
+    public static void testAtomicInteger() {
+        int i = 0;
+        AtomicInteger atomicInteger = new AtomicInteger(i);
+        i = atomicInteger.getAndIncrement();
+        log.info("i = {}", i);
+        log.info("i = {}", atomicInteger.get());
+        long second = TimeUnit.DAYS.toHours(1);
+        log.info("second = {}", second);
+        LongAdder longAdder = new LongAdder();
+        longAdder.increment();
+        longAdder.increment();
+        longAdder.increment();
+        log.info("longAdder = {}", longAdder);
 
+    }
 
+    public static void main(String[] args) {
+        testAtomicInteger();
+    }
 }
