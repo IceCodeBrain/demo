c8b257331fb9c83cd542ca27165d2d86fc5201e2
彭文彬 <925234379@qq.com>
2020-09-04 16:18:43 +0800

    学习Random

--- demo-two/src/main/java/com/example/demo/controller/BaseController.java
+++ demo-two/src/main/java/com/example/demo/controller/BaseController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DateUtils;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -33,13 +37,27 @@ public class BaseController {
 
     }
 
-    public static void test1(){
+    public static void test1() {
         Random random = new Random();
-        log.info("{}",random.nextInt(999) % (999 - 100 + 1) + 100);
+        log.info("{}", random.nextInt(999) % (999 - 100 + 1) + 100);
+    }
+
+
+    private static long getFakeSaleNum(Date now, Date startTime, Date endTime) {
+        log.info("计算假销量  now :{} ; startTime: {} ; endTime : {}; fakeStock : {}", now, startTime, endTime, 100);
+        long fakeSaleNum = new BigDecimal(100).multiply(new BigDecimal(getMinute(now) - getMinute(startTime)).divide(new BigDecimal(getMinute(endTime) - getMinute(startTime)), new MathContext(2))).longValue();
+        log.info("fakeSaleNum :{}", fakeSaleNum);
+        return fakeSaleNum;
+    }
+
+    private static long getMinute(Date date) {
+        return date.getTime() / 1000;
     }
 
     public static void main(String[] args) {
-       // testAtomicInteger();
-        test1();
+        // testAtomicInteger();
+        //test1();
+        Date now = new Date();
+        getFakeSaleNum(now, DateUtils.addHours(now, -1), DateUtils.addHours(now, 0));
     }
 }
