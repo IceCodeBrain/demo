40c1b84bf46461e53426570b13ba2429478e85dc
彭文彬 <925234379@qq.com>
2021-03-24 17:03:21 +0800

    添加分页查询参数

---
+++ demo-two/src/main/java/com/example/demo/boot/restful/RestDTO.java
@@ -0,0 +1,56 @@
+package com.example.demo.boot.restful;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.SneakyThrows;
+
+/**
+ * @description: RestDTO <br>
+ * @date: 2021/3/24 16:57 <br>
+ * @author: PWB <br>
+ */
+@ApiModel("分页查询参数 model ")
+public class RestDTO<T> {
+
+    @ApiModelProperty("第几页 默认1 ")
+    public Integer pageNum = 1;
+
+    @ApiModelProperty("每页大小 默认 20")
+    private Integer pageSize = 20;
+
+    @ApiModelProperty("其它查询参数 ")
+    private T data;
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+
+    @SneakyThrows
+    @Override
+    public String toString() {
+        ObjectMapper om = new ObjectMapper();
+        return om.writeValueAsString(this);
+    }
+}
