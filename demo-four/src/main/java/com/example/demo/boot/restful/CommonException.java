c5dfd1a39e53e95ff67a734b31a1d9a921e24a9f
彭文彬 <wuyin@123>
2021-12-23 14:02:46 +0800

    完善模板

---
+++ demo-four/src/main/java/com/example/demo/boot/restful/CommonException.java
@@ -0,0 +1,50 @@
+package com.example.demo.boot.restful;
+
+import java.io.Serializable;
+
+
+/**
+ * @description: 通用异常抛出 列如 工具类中的异常抛出  <br>
+ * @since: 1.0 <br>
+ * @date: 2020/10/21 11:21 <br>
+ * @author: PWB <br>
+ */
+public class CommonException extends RuntimeException implements Serializable {
+
+  private static final long serialVersionUID = 1213855733833039552L;
+
+  private Integer code;
+
+  public CommonException() {
+    super(RestCode.FAIL.getMsg());
+    this.code = RestCode.FAIL.getCode();
+  }
+
+  public CommonException(String message) {
+    super(message);
+    this.code = RestCode.FAIL.getCode();
+  }
+
+  public CommonException(Integer code, String message) {
+    super(message);
+    this.code = code;
+  }
+
+  public CommonException(RestCode resCode) {
+    super(resCode.getMsg());
+    this.code = resCode.getCode();
+  }
+
+  public CommonException(String message, Throwable cause) {
+    super(message, cause);
+    this.code = RestCode.FAIL.getCode();
+  }
+
+  public Integer getCode() {
+    return code;
+  }
+
+  public void setCode(Integer code) {
+    this.code = code;
+  }
+}
---
+++ demo-four/src/main/java/com/example/demo/boot/restful/ServiceException.java
@@ -0,0 +1,44 @@
+package com.example.demo.boot.restful;
+
+import java.io.Serializable;
+
+
+public class ServiceException extends RuntimeException implements Serializable {
+
+  private static final long serialVersionUID = 1213855733833039552L;
+
+  private Integer code;
+
+  public ServiceException() {
+    super(RestCode.FAIL.getMsg());
+    this.code = RestCode.FAIL.getCode();
+  }
+
+  public ServiceException(String message) {
+    super(message);
+    this.code = RestCode.FAIL.getCode();
+  }
+
+  public ServiceException(Integer code, String message) {
+    super(message);
+    this.code = code;
+  }
+
+  public ServiceException(RestCode resCode) {
+    super(resCode.getMsg());
+    this.code = resCode.getCode();
+  }
+
+  public ServiceException(String message, Throwable cause) {
+    super(message, cause);
+    this.code = RestCode.FAIL.getCode();
+  }
+
+  public Integer getCode() {
+    return code;
+  }
+
+  public void setCode(Integer code) {
+    this.code = code;
+  }
+}
